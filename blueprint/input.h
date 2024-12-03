10b8f5a85f46be93ea54e7ad83bc3f3ce558a237
TK <tilkoke@live.de>
2024-12-03 17:41:32 +0100

    Add reusable blueprint for each puzzle

---
+++ blueprint/input.h
@@ -0,0 +1,12 @@
+#include <string_view>
+constexpr auto example_input_literal = 
+R"(
+
+)";
+constexpr std::string_view example_input{example_input_literal};
+
+constexpr auto puzzle_input_literal = 
+R"(
+
+)";
+constexpr std::string_view puzzle_input{puzzle_input_literal};
---
+++ blueprint/main.cpp
@@ -0,0 +1,39 @@
+#include <string_view>
+#include <vector>
+#include <iostream>
+#include <ranges>
+#include <charconv>
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <queue>
+#include <thread>
+#include <chrono>
+#include "input.h"
+
+long parse(const std::string_view& input) {
+    using std::string_view;
+    using namespace std::views;
+
+    return input.size();
+}
+
+void timed_execution(const std::string_view& input, const std::string_view& title) {
+    auto start = std::chrono::system_clock::now();
+    int result = parse(input);
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    std::cout << title << std::endl;
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "Time needed: " << elapsed.count() << std::endl;
+}
+
+int main() 
+{
+    timed_execution(example_input, "Example input");
+    std::cout << "-------------------------------------------" << std::endl;
+    timed_execution(puzzle_input, "Puzzle input");
+
+    return 0;
+}  
