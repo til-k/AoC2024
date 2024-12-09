03dd58e67c038f562d4b1d31fcc06058572cd71d
TK <tilkoke@live.de>
2024-12-09 17:36:50 +0100

    Switch out old grid to new CharGrid2D class (faster by quite a bit!)

--- 06_02/main.cpp
+++ 06_02/main.cpp
@@ -10,9 +10,109 @@
 #include <thread>
 #include <chrono>
 #include <set>
+#include <functional>
 #include "input.h"
 
 enum dir{UP, DOWN, LEFT, RIGHT};
+
+struct IntVec2D {
+    long x = 0;
+    long y = 0;
+
+    IntVec2D operator+(const IntVec2D& other) const {
+        return IntVec2D(x + other.x, y + other.y);
+    }
+
+    IntVec2D operator-(const IntVec2D& other) const {
+        return IntVec2D(x - other.x, y - other.y);
+    }
+
+    IntVec2D operator*(const auto& scalar) const {
+        return IntVec2D(x * scalar, y * scalar);
+    }
+
+    bool operator==(const IntVec2D& other) const {
+        return (x == other.x && y == other.y);
+    }
+
+    bool operator<(const IntVec2D& other) const {
+        return (x < other.x && y <= other.y);
+    }
+
+    void dbg_print() const{
+        std::cout << "x: " << x << ", y: " << y << std::endl;
+    }
+};
+
+class CharGrid2D {
+    private:
+        std::vector<char> grid;
+        IntVec2D dims;
+
+        CharGrid2D(const std::string_view& input) {
+            for(auto line : input | std::views::split('\n')) {
+                if(line.empty()) continue;
+                for(const char& c: line) {
+                    grid.push_back(c);
+                }
+                dims.y++;
+            }
+            if(dims.y > 0)
+                dims.x = grid.size() / dims.y;
+        }
+
+    public:
+        CharGrid2D(const IntVec2D& dims, const char& initializing_character) {
+            this->dims = dims;
+            this->grid = std::vector<char>(dims.x * dims.y, initializing_character);
+        }
+
+        static CharGrid2D from_input(const std::string_view& input) {
+            CharGrid2D grid(input);
+            return grid;
+        }
+
+        IntVec2D& get_dims() {
+            return dims;
+        }
+
+        void iterate_over(const std::function<void(const IntVec2D& pos, const char& c)> callback) {
+            for(long x = 0; x < dims.x; x++) {
+                for(long y = 0; y < dims.y; y++) {
+                    auto pos = IntVec2D(x, y);
+                    callback(pos, at(pos));
+                }
+            }
+        }
+
+        char at(IntVec2D pos) const {
+            if(pos.x < 0) return '\0';
+            if(pos.y < 0) return '\0';
+            if(pos.x >= dims.x) return '\0';
+            if(pos.y >= dims.y) return '\0';
+            return grid[pos.x + dims.x * pos.y];
+        }
+
+        char place(IntVec2D pos, char c) {
+            if(pos.x < 0) return '\0';
+            if(pos.y < 0) return '\0';
+            if(pos.x >= dims.x) return '\0';
+            if(pos.y >= dims.y) return '\0';
+            char current_char = grid[pos.x + dims.x * pos.y];
+            grid[pos.x + dims.x * pos.y] = c;
+            return current_char;
+        }
+
+        void dbg_print() {
+            std::cout << "Dimensions: x: " << dims.x << ", y: " << dims.y << std::endl;
+            for(long y = 0; y < dims.y; y++) {
+                for(long x = 0; x < dims.x; x++) {
+                    std::cout << at(IntVec2D(x, y));
+                }
+                std::cout << std::endl;
+            }
+        }
+};
 
 dir turn_right(dir direction) {
     switch(direction) {
@@ -28,49 +128,41 @@ dir turn_right(dir direction) {
     return direction;
 }
 
-std::pair<long, long> calc_next_position(const std::pair<long, long>& guard_position, const dir d) {
-    std::pair<long, long> next_position(guard_position);
+IntVec2D calc_next_position(const IntVec2D& guard_position, const dir d) {
+    IntVec2D next_position(guard_position);
     switch(d) {
         case UP:
-            next_position.second--;
+            next_position.y--;
             break;
         case DOWN:
-            next_position.second++;
+            next_position.y++;
             break;
         case LEFT:
-            next_position.first--;
+            next_position.x--;
             break;
         case RIGHT:
-            next_position.first++;
+            next_position.x++;
             break;
     }
     return next_position;
 }
 
-char look_at_next_step_on_map(const std::vector<std::vector<char>>& grid, const std::pair<long, long> guard_position, const dir d) {
-    using std::pair;
-    pair<long, long> next_position = calc_next_position(guard_position, d);
-    if(next_position.first < 0) return 'X';
-    if(next_position.second < 0) return 'Y';
-    if((ulong)next_position.second >= grid.size()) return 'F';
-    if((ulong)next_position.first >= grid[next_position.second].size()) return 'G';
-    if(grid[next_position.second][next_position.first] == 'E') return 'E';
-    if(grid[next_position.second][next_position.first] == '#') return '#';
-    if(grid[next_position.second][next_position.first] == '.') return '.';
-    return '?';
+char look_at_next_step_on_map(const CharGrid2D& grid, const IntVec2D guard_position, const dir d) {
+    IntVec2D next_position = calc_next_position(guard_position, d);
+    return grid.at(next_position);
 }
 
-bool contains_loop(const std::vector<std::vector<char>>& grid, const std::pair<long, long>& guard_start_position, const dir d) {
+bool contains_loop(const CharGrid2D& grid, const IntVec2D& guard_start_position, const dir d) {
     using std::vector;
     using std::tuple;
     using std::set;
     
     bool running = true;
     dir current_direction(d);
-    std::pair<long, long> guard_current_position(guard_start_position);
+    IntVec2D guard_current_position(guard_start_position);
     std::set<std::tuple<long, long, dir>> visited_positions_with_dir;
     while(running) {
-        auto pdt = std::tuple(guard_current_position.first, guard_current_position.second, current_direction); //position and direction tuple
+        auto pdt = std::tuple(guard_current_position.x, guard_current_position.y, current_direction); //position and direction tuple
         if(visited_positions_with_dir.find(pdt) != visited_positions_with_dir.end()) return true; //guard was already at that position with direction, must be loop
         visited_positions_with_dir.insert(pdt);
         auto next_step = look_at_next_step_on_map(grid, guard_current_position, current_direction);
@@ -95,31 +187,20 @@ long parse(const std::string_view& input) {
     using std::vector;
     using std::pair;
 
-    vector<vector<char>> grid(1000, vector<char>(1000, 'E')); // simply initiate a giant grid
-    pair<long, long> guard_start_position{0, 0};
-
-    pair<long, long> map_cursor{0, 0};
-    for(const auto& line : input | split(string_view{"\n"})) {
-        if(line.empty()) continue;
-        for(char c : line) {
-            if(c == '#') grid[map_cursor.second][map_cursor.first] = '#';
-            if(c == '.') grid[map_cursor.second][map_cursor.first] = '.';
-            if(c == '^') {
-                guard_start_position.first = map_cursor.first;
-                guard_start_position.second = map_cursor.second;
-                grid[map_cursor.second][map_cursor.first] = '.';
-            }
-            map_cursor.first++;
+    CharGrid2D grid = CharGrid2D::from_input(input); // simply initiate a giant grid
+    IntVec2D guard_start_position;
+    grid.iterate_over([&guard_start_position](const IntVec2D& pos, const char& c) {
+        if(c == '^') {
+            guard_start_position = pos;
         }
-        map_cursor.second++;
-        map_cursor.first = 0;
-    }
+    });
+    grid.place(guard_start_position, '.');
 
     long cnt_looped_maps = 0;
 
     bool running = true;
     dir current_direction = UP;
-    pair<long, long> guard_current_position(guard_start_position);
+    IntVec2D guard_current_position(guard_start_position);
     while(running) { //Note: expecting that the base map does not loop
         auto next_step = look_at_next_step_on_map(grid, guard_current_position, current_direction);
         switch(next_step) {
@@ -128,8 +209,8 @@ long parse(const std::string_view& input) {
                 break;
             case '.': {
                 const auto next_position = calc_next_position(guard_current_position, current_direction);
-                vector<vector<char>> grid_variation{grid};
-                grid_variation[next_position.second][next_position.first] = '#';
+                CharGrid2D grid_variation(grid);
+                grid_variation.place(next_position, '#');
                 if(contains_loop(grid_variation, guard_start_position, UP)) cnt_looped_maps++;
                 guard_current_position = next_position;
                 break;
