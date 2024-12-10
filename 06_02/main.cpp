3cfba9c57559dfcbaf5fa0e1265b158ad21e1941
TK <tilkoke@live.de>
2024-12-10 17:42:28 +0100

    Change day 6 part 2 to use helper classes and other small improvements

--- 06_02/main.cpp
+++ 06_02/main.cpp
@@ -1,6 +1,7 @@
 #include <string_view>
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <ranges>
 #include <charconv>
 #include <algorithm>
@@ -11,117 +12,10 @@
 #include <chrono>
 #include <set>
 #include <functional>
-#include "input.h"
+#include "../helpers/grid.h"
+#include "../helpers/aux.h"
 
 enum dir{UP, DOWN, LEFT, RIGHT};
-
-struct IntVec2D {
-    long x = 0;
-    long y = 0;
-
-    IntVec2D operator+(const IntVec2D& other) const {
-        return IntVec2D(x + other.x, y + other.y);
-    }
-
-    IntVec2D operator-(const IntVec2D& other) const {
-        return IntVec2D(x - other.x, y - other.y);
-    }
-
-    IntVec2D operator*(const auto& scalar) const {
-        return IntVec2D(x * scalar, y * scalar);
-    }
-
-    bool operator==(const IntVec2D& other) const {
-        return (x == other.x && y == other.y);
-    }
-
-    bool operator<(const IntVec2D& other) const {
-        return (x < other.x && y <= other.y);
-    }
-
-    void dbg_print() const{
-        std::cout << "x: " << x << ", y: " << y << std::endl;
-    }
-};
-
-class CharGrid2D {
-    private:
-        std::vector<char> grid;
-        IntVec2D dims;
-
-        CharGrid2D(const std::string_view& input) {
-            for(auto line : input | std::views::split('\n')) {
-                if(line.empty()) continue;
-                for(const char& c: line) {
-                    grid.push_back(c);
-                }
-                dims.y++;
-            }
-            if(dims.y > 0)
-                dims.x = grid.size() / dims.y;
-        }
-
-    public:
-        CharGrid2D(const IntVec2D& dims, const char& initializing_character) {
-            this->dims = dims;
-            this->grid = std::vector<char>(dims.x * dims.y, initializing_character);
-        }
-
-        bool operator==(const CharGrid2D& other) const {
-            return dims==other.dims && grid==other.grid;
-        }
-
-        bool operator<(const CharGrid2D& other) const {
-            if(dims!=other.dims) return dims < other.dims;
-            else return grid < other.grid;
-        }
-
-        static CharGrid2D from_input(const std::string_view& input) {
-            CharGrid2D grid(input);
-            return grid;
-        }
-
-        IntVec2D get_dims() const {
-            return dims;
-        }
-
-        void iterate_over(const std::function<void(const IntVec2D& pos, const char& c)> callback) {
-            for(long x = 0; x < dims.x; x++) {
-                for(long y = 0; y < dims.y; y++) {
-                    auto pos = IntVec2D(x, y);
-                    callback(pos, at(pos));
-                }
-            }
-        }
-
-        char at(IntVec2D pos) const {
-            if(pos.x < 0) return '\0';
-            if(pos.y < 0) return '\0';
-            if(pos.x >= dims.x) return '\0';
-            if(pos.y >= dims.y) return '\0';
-            return grid[pos.x + dims.x * pos.y];
-        }
-
-        char place(IntVec2D pos, char c) {
-            if(pos.x < 0) return '\0';
-            if(pos.y < 0) return '\0';
-            if(pos.x >= dims.x) return '\0';
-            if(pos.y >= dims.y) return '\0';
-            char current_char = grid[pos.x + dims.x * pos.y];
-            grid[pos.x + dims.x * pos.y] = c;
-            return current_char;
-        }
-
-        void dbg_print() const {
-            std::cout << "Dimensions: x: " << dims.x << ", y: " << dims.y << std::endl;
-            for(long y = 0; y < dims.y; y++) {
-                for(long x = 0; x < dims.x; x++) {
-                    std::cout << at(IntVec2D(x, y));
-                }
-                std::cout << std::endl;
-            }
-        }
-};
 
 dir turn_right(dir direction) {
     switch(direction) {
@@ -221,7 +115,9 @@ long parse(const std::string_view& input) {
                 const auto next_position = calc_next_position(guard_current_position, current_direction);
                 CharGrid2D grid_variation(grid);
                 grid_variation.place(next_position, '#');
-                if(contains_loop(grid_variation, guard_start_position, UP)) unique_looped_maps.insert(grid_variation);
+                if(unique_looped_maps.find(grid_variation) == unique_looped_maps.end()) //only attempt if maps not in the set already
+                    if(contains_loop(grid_variation, guard_start_position, UP)) 
+                        unique_looped_maps.insert(grid_variation);
                 guard_current_position = next_position;
                 break;
             }
@@ -234,22 +130,9 @@ long parse(const std::string_view& input) {
     return unique_looped_maps.size();
 }
 
-void timed_execution(const std::string_view& input, const std::string_view& title) {
-    auto start = std::chrono::system_clock::now();
-    int result = parse(input);
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-    std::cout << title << std::endl;
-    std::cout << "Result: " << result << std::endl;
-    std::cout << "Time needed: " << elapsed.count() << std::endl;
-}
-
 int main() 
 {
-    timed_execution(example_input, "Example input");
-    std::cout << "-------------------------------------------" << std::endl;
-    timed_execution(puzzle_input, "Puzzle input");
-
+    timed_execution(&parse, std::string_view((std::stringstream() << std::ifstream("sample.txt").rdbuf()).str()), "Example input");
+    timed_execution(&parse, std::string_view((std::stringstream() << std::ifstream("puzzle.txt").rdbuf()).str()), "Puzzle input");
     return 0;
 }  
